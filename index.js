2d0524c3b77c90770b89820fbb09503bd63346e3
KalebGz <kaleb.zeray@gmail.com>
2020-07-08 10:05:33 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    // txt = document.querySelector("p#text")
+    document.getElementById("text")
+         .innerHTML = "This is really cool!"
+})
